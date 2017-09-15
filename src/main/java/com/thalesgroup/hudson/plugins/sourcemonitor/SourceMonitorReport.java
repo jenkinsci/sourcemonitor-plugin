03c95082476812728a0a83fe65fd8ada478c3133
Rachel Peters <rachelp@priceindustries.com>
2017-09-15 16:12:35 -0500

    refactored class architecture

--- src/main/java/com/thalesgroup/hudson/plugins/sourcemonitor/SourceMonitorReport.java
+++ src/main/java/com/thalesgroup/hudson/plugins/sourcemonitor/SourceMonitorReport.java
@@ -141,39 +141,3 @@ public class SourceMonitorReport implements Serializable {
         return summaryMetrics;
     }
 }
-
-class FunctionStats{
-    private int complexity;
-    private int statements;
-    private String function;
-
-    public FunctionStats(int complexity, int statements, String function) {
-        this.complexity = complexity;
-        this.statements = statements;
-        this.function = function;
-    }
-
-    public int getComplexity() {
-        return complexity;
-    }
-
-    public void setComplexity(int complexity) {
-        this.complexity = complexity;
-    }
-
-    public int getStatements() {
-        return statements;
-    }
-
-    public void setStatements(int statements) {
-        this.statements = statements;
-    }
-
-    public String getFunction() {
-        return function;
-    }
-
-    public void setFunction(String function) {
-        this.function = function;
-    }
-}
